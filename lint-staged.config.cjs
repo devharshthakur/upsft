faf4cdf6fc4592f8e08816cdede7ce49b3d5d869
Harsh Thakur <harshprojects2002@outlook.com>
2026-04-17 16:04:25 +0530

    fix(`lint-staged`): `clippy` fix so lint suceeds

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   '*.{md,json,yaml,yml}': ['prettier --write'],
   '*.rs': [
-    (files) => `cargo fmt --check -- ${files.join(' ')}`,
-    () => 'cargo clippy --all-targets --all-features -- -D warnings',
-  ],
+  () => 'cargo fmt',
+  (files) => `cargo fmt --check -- ${files.join(' ')}`,
+  () => 'cargo clippy --all-targets --all-features',
+],
 };
