8626ed015ccd12d01922b583dca030ef1ce54c46
Harsh Thakur <harshprojects2002@outlook.com>
2026-04-18 15:25:13 +0530

    Update util.rs

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,13 @@
 use std::io;
 use std::process::{Command, Output};
 
-#[allow(dead_code)]
-pub fn execute_command(cmd: &'static str) -> io::Result<Output> {
+pub fn check_deps(dep: &str) -> bool {
+    execute_command(&format!("which {}", dep))
+        .map(|output| output.status.success())
+        .unwrap_or(false)
+}
+
+pub fn execute_command(cmd: &str) -> io::Result<Output> {
     let parts: Vec<&str> = cmd.split_whitespace().collect();
     if parts.is_empty() {
         return Err(io::Error::new(
