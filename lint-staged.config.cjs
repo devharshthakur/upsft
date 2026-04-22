c6466b8c5aa5ded6b808ba2ddad8369e58da5c01
Harsh Thakur <harshprojects2002@outlook.com>
2026-04-22 21:55:45 +0530

    fix: `lint-stage` config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
   "*.{md,json,yaml,yml}": ["prettier --write"],
   "*.rs": [
     () => "cargo fmt",
-    () => "cargo clippy --fix --all-targets --all-features",
+    () => "cargo clippy --fix --allow-dirty --all-targets --all-features",
   ],
 };
