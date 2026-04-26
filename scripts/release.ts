b1edb683685612d29e986f5ea39de664ff2f6180
Harsh Thakur <harshprojects2002@outlook.com>
2026-04-27 04:03:11 +0530

    chore: update `release.ts`

--- scripts/release.ts
+++ scripts/release.ts
@@ -5,4 +5,4 @@ exec("git add CHANGELOG.md");
 exec('git commit -m "chore: update changelog"');
 exec("git push");
 
-console.log("Changelog committed and pushed. Bump versions in Cargo.toml and package.json, then run pnpm release:tag.");
+console.log("Changelog committed and pushed. Bump versions in Cargo.toml, then run pnpm tag.");
